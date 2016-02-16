7fd853c42a9361b202ce288ec728cbfef7e179f3
Atrigger <mazahakaforever@ya.ru>
2016-02-16 13:14:27 +0300

    krotkov optimization

--- src/ru/ifmo/ctddev/kamenev/walk/RecursiveWalk.java
+++ src/ru/ifmo/ctddev/kamenev/walk/RecursiveWalk.java
@@ -2,43 +2,26 @@ package ru.ifmo.ctddev.kamenev.walk;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.security.MessageDigest;
 
 import static java.nio.file.FileVisitResult.CONTINUE;
 import static java.nio.file.FileVisitResult.TERMINATE;
 
-/**
- * Created by kamenev on 10.02.16.
- */
 public class RecursiveWalk {
     protected Path[] files;
 
     public static void main(String[] args) {
-        if (args.length < 2) {
-            System.out.println("No input file specified");
-            return;
-        }
-        RecursiveWalk walker = new RecursiveWalk();
-        try {
-            walker.initialize(args);
-        } catch (FileNotFoundException e) {
-            System.out.println(e);
-            return;
-        }
-        walker.process();
-    }
-
-    public void initialize(String[] input) throws FileNotFoundException {
-        files = new Path[2];
-        files[0] = Paths.get(input[0]);
-
-        files[1] = Paths.get(input[1]);
-        if (!files[0].toFile().exists()) {
-            throw new FileNotFoundException("Input file not found");
+        if (args.length == 2) {
+            RecursiveWalk walker = new RecursiveWalk();
+            walker.files = new Path[2];
+            walker.files[0] = Paths.get(args[0]);
+            walker.files[1] = Paths.get(args[1]);
+            walker.process();
         }
     }
 
@@ -57,41 +40,59 @@ public class RecursiveWalk {
         }
     }
 
+    public class Hasher {
+        public static final String MD5_ZERO = "00000000000000000000000000000000";
+
+        public String calculateMD5(Path filePath) {
+            try {
+                try (InputStream file = Files.newInputStream(filePath)) {
+                    byte[] dataBytes = new byte[1024];
+                    MessageDigest md = MessageDigest.getInstance("MD5");
+                    for (int n; (n = file.read(dataBytes)) != -1; )
+                        md.update(dataBytes, 0, n);
+                    byte[] mdbytes = md.digest();
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < mdbytes.length; i++)
+                        sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+                    return sb.toString().toUpperCase();
+                }
+            } catch (Exception e) { // Couldn't calculate hash
+                return MD5_ZERO;
+            }
+        }
+    }
+
     public class DirWalker
             extends SimpleFileVisitor<Path> {
         private final BufferedWriter output;
+        private Hasher hasher;
 
         DirWalker(BufferedWriter output) {
+            hasher = new Hasher();
             this.output = output;
         }
-        private FileVisitResult writeResult(String result, Path file){
+
+        private FileVisitResult writeResult(String result, Path file) {
             try {
-                output.write(result);
-                output.write(' ');
-                output.write(file.toString());
+                output.write(result + ' ' + file.toString());
                 output.newLine();
             } catch (IOException e) {
-                System.err.println(e);
                 return TERMINATE;
             }
             return CONTINUE;
         }
+
         @Override
         public FileVisitResult visitFile(Path file,
                                          BasicFileAttributes attr) {
-            return writeResult(Utils.calculateMD5(file),file);
-        }
-
-        @Override
-        public FileVisitResult postVisitDirectory(Path dir,
-                                                  IOException exc) {
-            return CONTINUE;
+            return writeResult(hasher.calculateMD5(file), file);
         }
 
         @Override
         public FileVisitResult visitFileFailed(Path file,
                                                IOException exc) {
-            return writeResult("00000000000000000000000000000000",file);
+            return writeResult(Hasher.MD5_ZERO, file);
         }
     }
 }
+
