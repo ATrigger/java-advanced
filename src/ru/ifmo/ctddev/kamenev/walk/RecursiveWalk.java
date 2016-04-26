5e72bb46ddb77c966acdc9958b9363a3a49efdae
Vladislav Kamenev <kamenev@Zephyr.itmo>
2016-04-26 11:50:43 +0300

    HW1 passed

--- src/ru/ifmo/ctddev/kamenev/walk/RecursiveWalk.java
+++ src/ru/ifmo/ctddev/kamenev/walk/RecursiveWalk.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
+import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.security.MessageDigest;
 
@@ -13,40 +14,42 @@ import static java.nio.file.FileVisitResult.CONTINUE;
 import static java.nio.file.FileVisitResult.TERMINATE;
 
 public class RecursiveWalk {
-    protected Path[] files;
-
     public static void main(String[] args) {
-        if (args.length == 2) {
+        if (args != null && args.length == 2) {
             RecursiveWalk walker = new RecursiveWalk();
-            walker.files = new Path[2];
-            walker.files[0] = Paths.get(args[0]);
-            if (Files.notExists(walker.files[0])) {
+            Path[] files = new Path[2];
+            try {
+                files[0] = Paths.get(args[0]);
+                files[1] = Paths.get(args[1]);
+            } catch (InvalidPathException e) {
+                System.out.println("Invalid path given");
+                return;
+            }
+            if (Files.notExists(files[0])) {
                 System.out.println("Input file doesn't exist");
                 return;
             }
-            walker.files[1] = Paths.get(args[1]);
-            walker.process();
-        } else System.err.println("No input\\output file specified");
-    }
-
-    public void process() {
-        try {
-            try (BufferedReader input = Files.newBufferedReader(files[0], StandardCharsets.UTF_8);
-                 BufferedWriter output = Files.newBufferedWriter(files[1], StandardCharsets.UTF_8)) {
-                for (String line; (line = input.readLine()) != null; ) {
-                    Path current = Paths.get(line);
-                    DirWalker dw = new DirWalker(output);
-                    Files.walkFileTree(current, dw);
+            try {
+                try (BufferedReader input = Files.newBufferedReader(files[0], StandardCharsets.UTF_8);
+                     BufferedWriter output = Files.newBufferedWriter(files[1], StandardCharsets.UTF_8)) {
+                    for (String line; (line = input.readLine()) != null; ) {
+                        Path current = Paths.get(line);
+                        DirWalker dw = walker.new DirWalker(output);
+                        Files.walkFileTree(current, dw);
+                    }
                 }
+            } catch (IOException e) {
+                System.err.println(e);
             }
-        } catch (IOException e) {
-            System.err.println(e);
-        }
+        } else System.err.println("No input\\output file specified");
     }
 
     public class Hasher {
         public static final String MD5_ZERO = "00000000000000000000000000000000";
-
+        private final HexBinaryAdapter adapter;
+        public Hasher() {
+            adapter = new HexBinaryAdapter();
+        }
         public String calculateMD5(Path filePath) {
             try {
                 try (InputStream file = Files.newInputStream(filePath)) {
@@ -54,10 +57,7 @@ public class RecursiveWalk {
                     MessageDigest md = MessageDigest.getInstance("MD5");
                     for (int n; (n = file.read(dataBytes)) != -1; )
                         md.update(dataBytes, 0, n);
-                    byte[] mdbytes = md.digest();
-                    StringBuilder sb = new StringBuilder();
-                    for (byte mdbyte : mdbytes) sb.append(Integer.toString((mdbyte & 0xff) + 0x100, 16).substring(1));
-                    return sb.toString().toUpperCase();
+                    return adapter.marshal(md.digest());
                 }
             } catch (Exception e) { // Couldn't calculate hash
                 return MD5_ZERO;
