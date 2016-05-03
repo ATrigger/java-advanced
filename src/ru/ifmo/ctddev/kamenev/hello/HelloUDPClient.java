46aec2429ed5f80c4ce865c9a135806823877a72
Your name <you@example.com>
2016-05-03 03:37:12 +0300

    HW9

---
+++ src/ru/ifmo/ctddev/kamenev/hello/HelloUDPClient.java
@@ -0,0 +1,24 @@
+package ru.ifmo.ctddev.kamenev.hello;
+
+/**
+ * Created by petrovich on 02.05.16.
+ */
+public class HelloUDPClient {
+
+    public HelloUDPClient(){
+    }
+    public void start(String host, int port, String prefix, int requests, int threads){
+
+    }
+    public static void main(String[] args) {
+        if(args.length != 5) {
+            System.out.println("Invalid amount of arguments provided");
+            return;
+        }
+        try {
+
+        } catch (){
+
+        }
+    }
+}
---
+++ src/ru/ifmo/ctddev/kamenev/hello/HelloUDPServer.java
@@ -0,0 +1,7 @@
+package ru.ifmo.ctddev.kamenev.hello;
+
+/**
+ * Created by petrovich on 02.05.16.
+ */
+public class HelloUDPServer {
+}
