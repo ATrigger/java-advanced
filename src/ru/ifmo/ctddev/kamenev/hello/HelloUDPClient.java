a5be9bbb49f94e9b7d5a7ddb3eb9f4f447c25757
Your name <you@example.com>
2016-05-03 04:17:44 +0300

    Tests passed

--- src/ru/ifmo/ctddev/kamenev/hello/HelloUDPClient.java
+++ src/ru/ifmo/ctddev/kamenev/hello/HelloUDPClient.java
@@ -1,24 +1,121 @@
 package ru.ifmo.ctddev.kamenev.hello;
 
+import java.io.IOException;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
- * Created by petrovich on 02.05.16.
+ * Class to perform sending packets on UDP protocol.
  */
 public class HelloUDPClient {
-
-    public HelloUDPClient(){
+    /**
+     * Creates instance of class.
+     */
+    public HelloUDPClient() {
     }
-    public void start(String host, int port, String prefix, int requests, int threads){
 
+    /**
+     * Runs a specified number of {@code threads} each sending number of {@code requests}
+     * to a server that is located on {@code host} and {@code port} that starts with {@code prefix}
+     *
+     * @param host     hostname or ip where server is running
+     * @param port     port number to send to
+     * @param prefix   prefix of requests
+     * @param requests number of requests
+     * @param threads  number of threads
+     */
+    public void start(String host, int port, String prefix, int requests, int threads) {
+        new Pimpl(host, port, prefix, requests, threads).action();
     }
+
+    private class Pimpl {
+        private final SocketAddress to;
+        private final String prefix;
+        private final int requests;
+        private final int threads;
+        private final ExecutorService workerPool;
+
+        public Pimpl(String host, int port, String prefix, int requests, int threads) {
+            this.requests = requests;
+            this.threads = threads;
+            this.prefix = prefix;
+            this.to = new InetSocketAddress(host, port);
+            this.workerPool = Executors.newFixedThreadPool(threads);
+        }
+
+        public void action() {
+            ArrayList<Callable<Void>> workers = new ArrayList<>();
+            for (int threadNum = 0; threadNum < threads; threadNum++) {
+                final int workerNum = threadNum;
+                workers.add(() -> {
+                    DatagramSocket threadSocket = new DatagramSocket();
+                    threadSocket.setSoTimeout(10000);
+                    final int socketBuff = threadSocket.getReceiveBufferSize();
+                    DatagramPacket pack = new DatagramPacket(new byte[socketBuff], socketBuff);
+                    for (int i = 0; i < requests; i++) {
+                        String request = prefix + workerNum + "_" + i;
+                        byte data[] = request.getBytes();
+                        DatagramPacket req = new DatagramPacket(data, data.length, to);
+                        while (!Thread.interrupted()) {
+                            try {
+                                threadSocket.send(req);
+                                try {
+                                    threadSocket.receive(pack);
+                                    String responseStr = new String(pack.getData(), pack.getOffset(), pack.getLength());
+                                    String referenceStr = "Hello, " + request;
+                                    if (!responseStr.equals(referenceStr)) {
+                                        continue;
+                                    }
+                                    System.out.println(responseStr);
+                                    break;
+                                } catch (IOException e) {
+                                    System.out.println("Unable to receive answer: " + e.getMessage());
+                                }
+                            } catch (IOException e) {
+                                System.out.println("Unable to send request: " + e.getMessage());
+                            }
+                        }
+                    }
+                    threadSocket.close();
+                    return null;
+                });
+            }
+            try {
+                workerPool.invokeAll(workers);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                workerPool.shutdown();
+            }
+        }
+    }
+
+    /**
+     *
+     * @param args args passed to command line
+     */
     public static void main(String[] args) {
-        if(args.length != 5) {
+        if (args.length != 5) {
             System.out.println("Invalid amount of arguments provided");
+            System.out.println("Usage: [host] [port] [prefix] [threads] [requests]");
             return;
         }
-        try {
-
-        } catch (){
-
+        String host = args[0];
+        String prefix = args[2];
+        try{
+            int port = Integer.parseInt(args[1]);
+            int threads = Integer.parseInt(args[3]);
+            int requests = Integer.parseInt(args[4]);
+            new HelloUDPClient().start(host,port,prefix,requests,threads);
+        } catch (NumberFormatException e){
+            System.out.println("Usage: [host] [port] [prefix] [threads] [requests]");
         }
     }
 }
--- src/ru/ifmo/ctddev/kamenev/hello/HelloUDPServer.java
+++ src/ru/ifmo/ctddev/kamenev/hello/HelloUDPServer.java
@@ -1,7 +1,115 @@
 package ru.ifmo.ctddev.kamenev.hello;
 
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.concurrent.*;
+
 /**
- * Created by petrovich on 02.05.16.
+ * Class that implements server that listens and responses to packets on UDP protocol.
  */
-public class HelloUDPServer {
+public class HelloUDPServer implements AutoCloseable {
+
+    private final BlockingQueue<Pimpl> runningServers;
+
+    /**
+     * Starts server on UDP protocol on specified port with specified number of threads.
+     *
+     * @param args arguments passed in command line
+     */
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Invalid amount of arguments provided");
+            System.out.println("Usage: [port] [threads]");
+            return;
+        }
+        try {
+            int port = Integer.parseInt(args[0]);
+            int threads = Integer.parseInt(args[1]);
+            HelloUDPServer server = new HelloUDPServer();
+            server.start(port, threads);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Usage: [port] [threads]");
+        }
+    }
+
+    private class Pimpl {
+        private final DatagramSocket socket;
+        private final ExecutorService workerThreads;
+        private final int buffSize;
+        private final int threads;
+
+        public Pimpl(int port, int threads) throws SocketException {
+            this.threads = threads;
+            this.socket = new DatagramSocket(port);
+            this.workerThreads = Executors.newFixedThreadPool(threads);
+            this.buffSize = socket.getReceiveBufferSize();
+        }
+
+        public void start() {
+            for (int i = 0; i < threads; i++) {
+                workerThreads.submit(() -> {
+                    DatagramPacket request = new DatagramPacket(new byte[buffSize], buffSize);
+                    while (!Thread.interrupted()) {
+                        try {
+                            socket.receive(request);
+                            String requestString = new String(request.getData(), request.getOffset(), request.getLength());
+                            String responseString = "Hello, " + requestString;
+
+                            byte[] data = responseString.getBytes();
+                            DatagramPacket response = new DatagramPacket(data, data.length, request.getAddress(), request.getPort());
+                            try {
+                                socket.send(response);
+                            } catch (IOException e) {
+                                System.out.println("Unable to send response: " + e.getMessage());
+                            }
+                        } catch (IOException e) {
+                            System.out.println("Failed to receive packet: " + e.getMessage());
+                        }
+                    }
+                });
+            }
+
+        }
+
+        public void close() {
+            workerThreads.shutdownNow();
+            socket.close();
+        }
+    }
+
+    /**
+     * Creates instance of class.
+     * Created class is ready to perform responding using  {@link #start(int, int)}
+     */
+    public HelloUDPServer() {
+        runningServers = new LinkedBlockingQueue<>();
+    }
+
+    /**
+     * Starts listening on {@code port} using {@code threads} number of threads.
+     * @param port port to listen to
+     * @param threads number of threads that listen
+     */
+    public void start(int port, int threads) {
+        try {
+            Pimpl server = new Pimpl(port, threads);
+            runningServers.add(server);
+            server.start();
+        } catch (SocketException e) {
+            System.out.println("Failed to start server: " + e.getMessage());
+        }
+
+    }
+
+    /**
+     * Closes all sockets and stops all threads.
+     */
+    @Override
+    public void close() {
+        runningServers.forEach(Pimpl::close);
+    }
 }
